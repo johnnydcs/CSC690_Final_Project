29953feb911739dcbb118d99f6cfa51a55739b66
johnnydcs <johnnydcs@yahoo.com>
2019-05-16 15:34:32 -0700

    add subjects n course numbers
    as titled

--- GatorGradsHelper/GatorGradsHelper/ViewController.swift
+++ GatorGradsHelper/GatorGradsHelper/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var SelectButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     
-    var Courses = ["CompSci", "Math", "English"]
+    var Subjects = ["Computer Science", "Math", "English", "Psychology", "Business"]
+    
+    var CourseNumbers = ["100", "110", "200", "210", "220", "256", "300", "412", "415", "510", "520", "600", "631", "645", "667"]
+    
+    var AddThisSubject = ""
+    var AddThisCourseNumber = ""
     
     @IBAction func SelectButtonDrop(_ sender: Any) {
         
@@ -47,17 +52,19 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Courses.count
+        return Subjects.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = Courses[indexPath.row]
+        cell.textLabel?.text = Subjects[indexPath.row]
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        SelectButton.setTitle("\(Courses[indexPath.row])", for: .normal)
+        SelectButton.setTitle("\(Subjects[indexPath.row])", for: .normal)
+        AddThisSubject = Subjects[indexPath.row]
+        AddThisCourseNumber = CourseNumbers[indexPath.row]
         animate(toggle: false)
     }
 }
