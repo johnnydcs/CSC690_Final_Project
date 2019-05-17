e5ed85e8298cd7ec9581e08808ba1f4afdee7d5b
johnnydcs <johnnydcs@yahoo.com>
2019-05-16 19:12:41 -0700

    Revert "removed 2nd table view"
    This reverts commit 13b039b204a6e1293471713d3f3ed8b7b959e10c.

--- GatorGradsHelper/GatorGradsHelper/ViewController.swift
+++ GatorGradsHelper/GatorGradsHelper/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var SelectNumButton: UIButton!
+    
+    @IBOutlet weak var tableNumView: UITableView!
+    
     var Subjects = ["Computer Science", "Math", "English", "Psychology", "Business"]
     
     var CourseNumbers = ["100", "110", "200", "210", "220", "256", "300", "412", "415", "510", "520", "600", "631", "645", "667"]
@@ -30,6 +34,15 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func SelectNumButtonDrop(_ sender: Any) {
+        
+        if tableNumView.isHidden {
+            animate2(toggle: true)
+        } else {
+            animate2(toggle: false)
+        }
+    }
+    
     func animate(toggle: Bool) {
         if (toggle) {
             UIView.animate(withDuration: 0.3) {
@@ -43,9 +56,23 @@ class ViewController: UIViewController {
         
     }
     
+    func animate2(toggle: Bool) {
+        if (toggle) {
+            UIView.animate(withDuration: 0.3) {
+                self.tableNumView.isHidden = false
+            }
+        } else {
+            UIView.animate(withDuration: 0.3) {
+                self.tableNumView.isHidden = true
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.isHidden = true
+        tableNumView.isHidden = true;
         // Do any additional setup after loading the view, typically from a nib.
     }
 }
@@ -66,6 +93,28 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         SelectButton.setTitle("\(Subjects[indexPath.row])", for: .normal)
         AddThisSubject = Subjects[indexPath.row]
+        AddThisCourseNumber = CourseNumbers[indexPath.row]
         animate(toggle: false)
     }
+    // End of Course Subject List
+    
+    // Start of Course Number List Only
+    func tableNumView(_ tableNumView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CourseNumbers.count
+    }
+    
+    func tableNumView(_ tableNumView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableNumView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath)
+        cell.textLabel?.text = CourseNumbers[indexPath.row]
+        return cell
+    }
+    
+    func tableNumView(_ tableNumView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        SelectNumButton.setTitle("\(CourseNumbers[indexPath.row])", for: .normal)
+        AddThisSubject = Subjects[indexPath.row]
+        AddThisCourseNumber = CourseNumbers[indexPath.row]
+        animate(toggle: false)
+    }
+    // End of Course Number List
+    
 }
